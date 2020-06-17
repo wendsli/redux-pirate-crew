8d94832ed22e75f6e11b0eb84eb54ff01c6c533f
wendsli <wesley.adams.davis@gmail.com>
2020-06-16 20:08:31 -0400

    displayed planked pirates and modified button to allow user to make them walk the plank

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ const initialState = {
     "Unicorn Nick",
     "Sergeant Fang",
     "Sharp-Eye Kerrin"
+  ],
+  plankers: [
+    "Commodore Brianna"
   ]
 }
 
 const ADD_PIRATE = 'ADD_PIRATE'
+const PLANK_PIRATE = 'PLANK_PIRATE'
 
 const newPirateForm = document.getElementById('new-pirate-form')
+const plankButton = document.getElementById('walk-the-plank')
 
 const crewReducer = (state = initialState, action) => {
   switch(action.type) {
@@ -18,6 +23,16 @@ const crewReducer = (state = initialState, action) => {
       const newCrewArray = state.crew.concat(action.newPirate)
       return Object.assign({}, state, {
         crew: newCrewArray
+      })
+    case PLANK_PIRATE:
+      const plankedPirate = state.crew[0]
+      const newPlankedPirates = state.plankers.concat(plankedPirate)
+      const filteredCrew = state.crew.filter((pirate) => {
+        return pirate !== plankedPirate
+      })
+      return Object.assign({}, state, {
+        crew: filteredCrew,
+        plankers: newPlankedPirates
       })
     default:
       return state;
@@ -31,11 +46,21 @@ const addPirateToCrew = (newPirate) => {
   }
 }
 
+const plankPirate = () => {
+  return {
+    type: PLANK_PIRATE
+  }
+}
+
 newPirateForm.addEventListener('submit', () => {
   event.preventDefault();
   const pirateName = document.getElementById('name').value
   document.getElementById('name').value = ''
   store.dispatch(addPirateToCrew(pirateName))
+})
+
+plankButton.addEventListener('click', () => {
+  store.dispatch(plankPirate())
 })
 
 const renderCrewList = () => {
@@ -47,10 +72,20 @@ const renderCrewList = () => {
   crewList.innerHTML = newCrewList
 }
 
+const renderPlankerList = () => {
+  const plankerList = document.getElementById('walked-crew')
+  let newPlankerList = ''
+  store.getState().plankers.forEach((planker) => {
+    newPlankerList += `<p>${planker}</p>`
+  });
+  plankerList.innerHTML = newPlankerList
+}
+
 const store = createStore(crewReducer);
 
 const render = () => {
   renderCrewList();
+  renderPlankerList();
 }
 
 render();
